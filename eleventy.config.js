285db5198eaa5f4d340129041f0eb4a3d5e63905
Jacob T. Fisher <jacob.taylor.fisher@gmail.com>
2022-01-17 14:39:30 -0600

    updated css

--- eleventy.config.js
+++ eleventy.config.js
@@ -22,35 +22,35 @@ module.exports = config => {
     require("./utilities/transforms/htmlminify")
   );
 
-  // Collections
-  config.addCollection("blog", (collection) => {
-    const blogs = collection.getFilteredByTag("blog");
+  // // Collections
+  // config.addCollection("blog", (collection) => {
+  //   const blogs = collection.getFilteredByTag("blog");
 
-    for (let i = 0; i < blogs.length; i++) {
-      const previous_post = blogs[i - 1];
-      const next_post = blogs[i + 1];
+  //   for (let i = 0; i < blogs.length; i++) {
+  //     const previous_post = blogs[i - 1];
+  //     const next_post = blogs[i + 1];
 
-      blogs[i].data["previous_post"] = previous_post;
-      blogs[i].data["next_post"] = next_post;
-    }
+  //     blogs[i].data["previous_post"] = previous_post;
+  //     blogs[i].data["next_post"] = next_post;
+  //   }
 
-    return blogs.reverse();
-  });
+  //   return blogs.reverse();
+  // });
 
-  // Categories collection
-  config.addCollection("categories", (collection) => {
-    const list = new Set();
+  // // Categories collection
+  // config.addCollection("categories", (collection) => {
+  //   const list = new Set();
 
-    collection.getAll().forEach((item) => {
-      if (!item.data.tags) return;
+  //   collection.getAll().forEach((item) => {
+  //     if (!item.data.tags) return;
 
-      item.data.tags
-        .filter((category) => !["blog", "all"].includes(category))
-        .forEach((category) => list.add(category));
-    });
+  //     item.data.tags
+  //       .filter((category) => !["blog", "all"].includes(category))
+  //       .forEach((category) => list.add(category));
+  //   });
 
-    return Array.from(list).sort();
-  });
+  //   return Array.from(list).sort();
+  // });
 
   // Shuffle filter
 
@@ -59,16 +59,23 @@ module.exports = config => {
     return helpers.shuffle(array);
   });
 
+  module.exports = function (eleventyConfig) {
+    eleventyConfig.ignores.add("_drafts/**");
+    eleventyConfig.ignores.add("layouts/blog.njk");
+    eleventyConfig.ignores.add("layouts/post.njk");
+    eleventyConfig.ignores.add("layouts/category.njk");
+  };
+
   // Layout aliases
   config.addLayoutAlias("base", "layouts/base.njk");
   config.addLayoutAlias("home", "layouts/home.njk");
   config.addLayoutAlias("page", "layouts/page.njk");
-  config.addLayoutAlias("blog", "layouts/blog.njk");
-  config.addLayoutAlias("post", "layouts/post.njk");
+  // config.addLayoutAlias("blog", "layouts/blog.njk");
+  // config.addLayoutAlias("post", "layouts/post.njk");
   config.addLayoutAlias("organizers", "layouts/organizers.njk");
   config.addLayoutAlias("sponsorship", "layouts/sponsorship.njk");
   config.addLayoutAlias("contact", "layouts/contact.njk");
-  config.addLayoutAlias("category", "layouts/category.njk");
+  // config.addLayoutAlias("category", "layouts/category.njk");
 
   // Include our static assets
   config.addPassthroughCopy("css");
